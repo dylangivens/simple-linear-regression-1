91f789c3156d9da89ce8656b242ab2965bbe7af8
Dylan Givens <d.t.givens@hotmail.com>
2024-02-07 12:27:20 -0500

    Read in data set, begin exploratory analysis

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-test
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', 200)
+
+df = pd.read_csv('C:/Users/dtgiv/Downloads/team.csv')
+print(df)
+
+print(df.columns)
+
+print(df.head())
